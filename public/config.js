6bba663b2ee23035754b232f38b3118b83932f08
themyrl <loic.themyr@lecnam.net>
2023-11-08 17:55:27 +0100

    proxy set up

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_CORS_PROXY_URL: "https://mystream-proxy.loic-themyr.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZGE5N2FjY2NkYmFmZDZkNTE3M2YxYjgyYTBlZjcwZCIsInN1YiI6IjY1NGJhMGFjNTMyYWNiNTMzNTNmOTFiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Dcw7lASkhxI-pi9nR-cVwnysjYLISrjXl2DjJwHBGP8"
 };
